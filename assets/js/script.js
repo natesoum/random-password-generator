d178bcb3fa0f497a05d3a78ebd7223f7f178d666
natesoum <soumnathun@gmail.com>
2022-07-21 23:42:01 -0400

    Completed README and generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperChars = ["A", "B", "C", "D", "E", "D", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ",", "]", "^", "_", "`", "{", "|", "}", "~"];
-var numbChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var userChoice;
+
 
 
 function generatePassword() {
-  var password = 'password';
+  var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperChars = ["A", "B", "C", "D", "E", "D", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ",", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var numbChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var userChoice = "";
+
   // TODO: add code to generate the password here
   // Asks for user input
 var userLen = prompt("How many characters would you like for your password? Please choose between 8 and 128 characters.");
 // If user does not input a number between 8 and 128
 if (userLen < 8 || userLen > 128) {
-  alert("Please enter a number between 8 and 128.")
+  alert("Please enter a number between 8 and 128.");
 } else {
   // Prompts users & ask if they would like lowercase characters in their password
   var useLower = confirm("Would you like your password to include lowercase characters?");
   // If user chooses yes, add all lowercase characters to userChoice
   if (useLower) {
-    // password/userChoice += lowerChars;
+    userChoice = lowerChars.concat(userChoice);
   };
   // Prompts user & ask if they would like uppercase characters in their password
   var useUpper = confirm("Would you like your password to include uppercase characters?");
   // If user chooses yes, add all uppercase characters to userChoice
   if (useUpper) {
-    // password/userChoice += upperChars;
+    userChoice = upperChars.concat(userChoice);
   };
   // Prompts user & ask if they would like special characters in their password
   var useSpecial = confirm("Would you like your password to include special characters?");
   // If user chooses yes, add all special characters to userChoice
   if (useSpecial) {
-    // password/userChoice += specChars;
+    userChoice = specChars.concat(userChoice);
   };
   // Prompts user & ask if they would like numbers in their password
   var useNums = confirm("Would you like your password to include numbers?");
   // If user chooses yes, add all numbers to userChoice
   if (useNums) {
-    // password/userChoice += numbers;
+    userChoice = numbChars.concat(userChoice);
   };
   // If user chooses no, they will be prompted that one character type must be chosen
-  if (lowerCase === false && upperCase === false && specChars === false && numbers === false) {
+  if (useLower === false && useUpper === false && useSpecial === false && useNums === false) {
     alert("Please select at least one character type.");
     generatePassword();
-      }
+  }
+
+
+  var password = "";
+  for (var i = 0; i < userLen; i++) {
+    password = password + userChoice[Math.floor(Math.random() * userChoice.length)];
+    }
 }
-
-// Steps
-// . User will click "Generate Password"
-// . User will input length > 8 & < 128
-// . User will choose between uppercase, lowercase, special characters & numbers
-// . Validate that user has chosen at least one type for their password
-// . Generate password based on user choices
-
-// . Display password on the screen or through prompt
   return password;
 }
 
